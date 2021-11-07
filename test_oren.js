7f5e85f441208195667f43239f277931725ca5eb
saberのmaster <278457198@qq.com>
2021-11-07 20:10:54 +0800

    v0.02

--- test_oren.js
+++ test_oren.js
@@ -6,47 +6,40 @@ function phone_oren_way(event){
     */
     const beta = event.beta,
         gamma = event.gamma;
-    strokeWeight(packageGap*0.52)
+    strokeWeight(packageGap*0.55)
     stroke('white')
     point(userPath.X, userPath.Y)
+    console.log(userPath)
     var index=point_index()
-    console.log(userPath.Y)
     // up and down
-    if(abs(beta)>abs(gamma)){
+    const thePackageWay=userPath.Y+packageGap*0.55<parseInt(index/mazelength+1)*packageGap+5
+    &&userPath.Y-packageGap*0.55>parseInt(max(index/mazelength-1,0))*packageGap+5
+    &&userPath.X+packageGap*0.55<parseInt(index%mazelength+1)*packageGap+5
+    &&userPath.X-packageGap*0.55>parseInt(max(index%mazelength-1,0))*packageGap+5
         // const packageGap=screenFace.height/15
         //down
         if(beta>0){
-            //no wall on down 
-            if(canWeGo(index,index+1)){
-                userPath.Y+=add_path(beta)
-            }else if(userPath.Y<parseInt(index/mazelength+1)*packageGap-0.1*packageGap){
+            //向下移动的时候，好像没有什么要特别关注的
+            if(thePackageWay||canWeGo(index,index+1)){
                 userPath.Y+=add_path(beta)
             }
         }//up
         else if(beta<0){
-            if(canWeGo(index,index-1)){
-                userPath.Y-=add_path(beta)
-            }else if(userPath.Y>(max(index/mazelength-1,0.5))*packageGap+0.1*packageGap){
+            //向上移动的时候，需要额外判断边界墙
+            if(thePackageWay||canWeGo(index,index-1)){
                 userPath.Y-=add_path(beta)
             }
         }
-    }// left and right
-    else{
+    // left and right
         if(gamma>0){
-            if(canWeGo(index,index+mazelength)){
-                userPath.X+=add_path(gamma)
-            }else if(userPath.X<parseInt(index%mazelength+1)*packageGap-0.1*packageGap){
+            if(thePackageWay||canWeGo(index,index+mazelength)){
                 userPath.X+=add_path(gamma)
             }
         }else if(gamma<0){
-            if(canWeGo(index,index-mazelength)){
-                userPath.X-=add_path(gamma)
-            }else if(userPath.X>max(index%mazelength-1,0.5)*packageGap+0.1*packageGap){
+            if(thePackageWay||canWeGo(index,index-mazelength)){
                 userPath.X-=add_path(gamma)
             }
-
         }
-    }
 
 }
 
@@ -63,7 +56,7 @@ function point_index(){
 }
 
 function add_path(area){
-    return parseInt(packageGap/90*abs(area))
+    return parseInt(0.55*packageGap/90*abs(area))
 }
 
 window.addEventListener("deviceorientation",phone_oren_way)
